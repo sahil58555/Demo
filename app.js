8a964a3841ae325d5f0b63cd45e0e1381a15912d
sahil58555 <sahil.saxena.58555@gmail.com>
2023-05-09 20:52:19 +0530

    commit

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const express = require('express')
+const express = require("express");
 const app = express();
+const data = require("./data.js");
 
-app.get('/', (req, res) => {
-    res.send('hi from server');
-})
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    data,
+  });
+});
+
+app.post("/", (req, res) => {
+  const user = req.body;
+  data.push(user);
+  res.status(200).send("Done");
+});
 
 app.listen(3000, () => {
-    console.log('Server is running');
-})
+  console.log("Server is running");
+});
---
+++ data.js
@@ -0,0 +1,16 @@
+const data = [
+    {
+        id:1,
+        name: 'sahil'
+    },
+    {
+        id:2,
+        name: 'mohan'
+    },
+    {
+        id:3,
+        name: 'rohan'
+    }
+]
+
+module.exports = data;
